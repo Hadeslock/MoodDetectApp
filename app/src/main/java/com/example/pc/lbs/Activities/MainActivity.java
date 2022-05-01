e966a409c1055554b34a3e34c647e37a86444a2e
Hades <hadeslock@126.com>
2022-05-01 19:05:38 +0800

    重构首页UI，改成底部导航栏的模式

--- app/src/main/java/com/example/pc/lbs/Activities/MainActivity.java
+++ app/src/main/java/com/example/pc/lbs/Activities/MainActivity.java
@@ -1,23 +1,19 @@
 package com.example.pc.lbs.Activities;
 
 import android.Manifest;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import com.example.pc.lbs.R;
 import com.example.pc.lbs.TheUtils.FileUtils;
 import com.example.pc.lbs.permission.PermissionHelper;
 import com.example.pc.lbs.permission.PermissionInterface;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /**
  * Author: Hadeslock
@@ -41,71 +37,15 @@ public class MainActivity extends AppCompatActivity implements PermissionInterfa
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // 创建 DataList.txt 文件
-        FileUtils.makeFilePath(FileUtils.getSDCardPath() + "/bletest/",
-                "DataList.txt");
+        
+        //设置底部导航栏控制fragment显示
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupWithNavController(navView, navController);
 
         //动态获取权限
         mPermissionHelper = new PermissionHelper(this, this);
         mPermissionHelper.requestPermissions();
-    }
-
-    // 初始化界面引用
-    private void initView() {
-        //初始化组件引用
-        FunctionList = findViewById(R.id.functionList);
-        //设置组件回调
-        FunctionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Class<?> TargetClass = null;
-                switch (position) {
-                    case 0:  // 跳转蓝牙连接并连接
-                        TargetClass = ScanDeviceActivity.class;
-                        break;
-                    case 1: //绑定设备
-                        TargetClass = BindDeviceActivity.class;
-                        break;
-                    case 2:   // 跳转查看记录
-                        TargetClass = TestActivity.class;
-                        break;
-                    case 3:
-                        //查看未上传的记录
-                        TargetClass = ViewUnuploadRecordActivity.class;
-                        break;
-                    case 4: //注销登录
-                        SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
-                        editor.putBoolean("isLogin", false);
-                        editor.apply();
-                        TargetClass = LoginActivity.class;
-                        break;
-                    default:
-                        break;
-                }
-                if (TargetClass != null) {
-                    Intent intent = new Intent(MainActivity.this, TargetClass);
-                    if (TargetClass == ScanDeviceActivity.class) {
-                        intent.putExtra("fromActivity", INTENT_SCAN_DEVICE_FOR_MEASURE);
-                    }
-                    intent.putExtra("from", 0);//不知道这个数据是哪里用的，不敢删
-                    startActivity(intent);
-                    //如果是要跳转到登录界面，说明是注销操作，本页面需要被关闭
-                    if (TargetClass == LoginActivity.class) {
-                        finish();
-                    }
-                }
-            }
-        });
-        //设置列表的数据源
-        List<String> data = new ArrayList<>();
-        data.add("连接设备");
-        data.add("绑定设备");
-        data.add("查看记录并获取结果");
-        data.add("查看未上传的记录");
-        data.add("注销登录");
-        FunctionList.setAdapter(new ArrayAdapter<>(
-                this, android.R.layout.simple_expandable_list_item_1, data));
     }
 
     // --------------------- 动态权限校验回调开始 ---------------------
@@ -138,7 +78,9 @@ public class MainActivity extends AppCompatActivity implements PermissionInterfa
     @Override
     public void requestPermissionsSuccess() {
         //所有权限都已满足，可以进行界面操作
-        initView(); //初始化界面引用
+        // 创建 DataList.txt 文件
+        FileUtils.makeFilePath(FileUtils.getSDCardPath() + "/bletest/",
+                "DataList.txt");
     }
 
     @Override
