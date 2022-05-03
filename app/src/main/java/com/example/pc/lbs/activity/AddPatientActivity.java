4450c5d0bddddc6be76cbbc25ae3818d69d8b7a9
Hades <hadeslock@126.com>
2022-05-03 20:53:13 +0800

    添加病人时住址的输入改为手动

--- app/src/main/java/com/example/pc/lbs/activity/AddPatientActivity.java
+++ app/src/main/java/com/example/pc/lbs/activity/AddPatientActivity.java
@@ -1,26 +1,24 @@
 package com.example.pc.lbs.activity;
 
-import android.Manifest;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.*;
-import com.baidu.location.BDAbstractLocationListener;
-import com.baidu.location.BDLocation;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import com.example.pc.lbs.BuildConfig;
 import com.example.pc.lbs.R;
-import com.example.pc.lbs.utils.GsonUtil;
-import com.example.pc.lbs.utils.HttpUtil;
-import com.example.pc.lbs.permission.PermissionHelper;
-import com.example.pc.lbs.permission.PermissionInterface;
 import com.example.pc.lbs.pojo.Patient;
 import com.example.pc.lbs.pojo.RespBean;
+import com.example.pc.lbs.utils.GsonUtil;
+import com.example.pc.lbs.utils.HttpUtil;
+import com.lljjcoder.Interface.OnCityItemClickListener;
+import com.lljjcoder.bean.CityBean;
+import com.lljjcoder.bean.DistrictBean;
+import com.lljjcoder.bean.ProvinceBean;
+import com.lljjcoder.style.cityjd.JDCityConfig;
+import com.lljjcoder.style.cityjd.JDCityPicker;
 import okhttp3.Call;
 import okhttp3.RequestBody;
 import okhttp3.Response;
@@ -35,41 +33,40 @@ import java.io.IOException;
  */
 
 public class AddPatientActivity extends AppCompatActivity
-        implements View.OnClickListener, PermissionInterface {
+        implements View.OnClickListener {
 
     private static final String TAG = "AddPatientActivity";
 
-    //消息代码
+    // region 消息代码
     private static final int MSG_ADD_PATIENT_SUCCESS = 1;
     private static final int MSG_ADD_PATIENT_FAILURE = 2;
     private static final int MSG_NETWORK_FAILURE = 3;
     private static final int RES_ADD_PATIENT_SUCCESS = 1; //成功添加病人的响应码
+    // endregion
 
-    //界面组件引用
-    private EditText addPatientName;
+    // region 界面组件引用
+    private EditText addPatientName; //
     private EditText addPatientAge;
     private RadioGroup addPatientGender;
-    private TextView addPatientPosition;
+    private TextView addPatientLocation;
+    private EditText addPatientAddress;
     private EditText addPatientIdentity;
     private Button addPatientAddBtn;
+    // endregion
+
+    //城市选择器
+    JDCityPicker cityPicker = new JDCityPicker();
+    JDCityConfig jdCityConfig = new JDCityConfig.Builder().build();
 
     private String selectGender; //当前选择的性别 男-0 女-1
-    private String curPosition = "未获取地址"; //当前位置
-
-    //定位相关
-    private LocationClient mLocationClient; //定位服务客户端
-
-    //动态权限
-    private PermissionHelper mPermissionHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_patient);
 
-        //获取权限
-        mPermissionHelper = new PermissionHelper(this, this);
-        mPermissionHelper.requestPermissions();
+        initView();
+        initEvent();
     }
 
     /*
@@ -82,11 +79,14 @@ public class AddPatientActivity extends AppCompatActivity
         addPatientName = findViewById(R.id.add_patient_name);
         addPatientAge = findViewById(R.id.add_patient_age);
         addPatientGender = findViewById(R.id.add_patient_gender);
-        addPatientPosition = findViewById(R.id.add_patient_position);
+        addPatientLocation = findViewById(R.id.add_patient_location);
+        addPatientAddress = findViewById(R.id.add_patient_address);
         addPatientIdentity = findViewById(R.id.add_patient_identity);
         addPatientAddBtn = findViewById(R.id.add_patient_addBtn);
+    }
 
-        //设置点击事件回调
+    //初始化事件
+    private void initEvent() {
         addPatientAddBtn.setOnClickListener(this);
         addPatientGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -95,30 +95,26 @@ public class AddPatientActivity extends AppCompatActivity
                 selectGender = checked.getText().toString();
             }
         });
-    }
+        addPatientLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jdCityConfig.setShowType(JDCityConfig.ShowType.PRO_CITY_DIS);
+                cityPicker.init(AddPatientActivity.this);
+                cityPicker.setConfig(jdCityConfig);
+                cityPicker.setOnCityItemClickListener(new OnCityItemClickListener() {
+                    @Override
+                    public void onSelected(ProvinceBean province, CityBean city, DistrictBean district) {
+                        String cityResult = province.getName() + city.getName() + district.getName();
+                        addPatientLocation.setText(cityResult);
+                    }
 
-    /*
-     * 初始化定位服务
-     * @author Hadeslock
-     * @time 2022/4/13 20:36
-     */
-    private void initLocationService() {
-        //初始化客户端
-        mLocationClient = new LocationClient(getApplicationContext());
-        mLocationClient.registerLocationListener(new MyLocationListener());
-        //定位选项
-        LocationClientOption option = new LocationClientOption();
-        //可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
-        option.setScanSpan(5000);
-        //可选，设置是否需要地址信息，默认不需要
-        option.setIsNeedAddress(true);
-        //可选，是否需要位置描述信息，默认为不需要，即参数为false
-        //如果开发者需要获得当前点的位置信息，此处必须为true
-        option.setIsNeedLocationDescribe(true);
-        //需将配置好的LocationClientOption对象，通过setLocOption方法传递给LocationClient对象使用
-        mLocationClient.setLocOption(option);
-        //启动服务
-        mLocationClient.start();
+                    @Override
+                    public void onCancel() {
+                    }
+                });
+                cityPicker.showCityPicker();
+            }
+        });
     }
 
     @Override
@@ -131,7 +127,8 @@ public class AddPatientActivity extends AppCompatActivity
             int age = Integer.parseInt(addPatientAge.getText().toString());
             int gender = "男".equals(selectGender) ? 0 : 1;
             String identity = addPatientIdentity.getText().toString();
-            Patient patient = new Patient(name, age, gender, curPosition, identity);
+            String position = addPatientLocation.getText().toString() + addPatientAddress.getText().toString();
+            Patient patient = new Patient(name, age, gender, position, identity);
             //生成请求参数
             RequestBody requestBody = GsonUtil.generateRequestBody(patient, "json");
             String addPatientUrl = BuildConfig.baseUrl + "patient/addPatient";
@@ -161,18 +158,6 @@ public class AddPatientActivity extends AppCompatActivity
         }
     }
 
-    //地图服务监听，重写回调以更新ui
-    public class MyLocationListener extends BDAbstractLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation bdLocation) {
-            //设置地址信息
-            curPosition = bdLocation.getAddrStr();
-            addPatientPosition.setText(curPosition);
-            Log.d(TAG, "onReceiveLocation: 获取到地址" + curPosition
-                    + "\n代码：" + bdLocation.getLocType());
-        }
-    }
-
     //消息回调
     private final Handler mHandler = new Handler(new Handler.Callback() {
         @Override
@@ -192,43 +177,4 @@ public class AddPatientActivity extends AppCompatActivity
             return true;
         }
     });
-
-    // --------------------- 动态权限校验回调开始 ---------------------
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (mPermissionHelper.requestPermissionsResult(requestCode, permissions, grantResults)) {
-            //权限请求结果，并已经处理了该回调
-            return;
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    }
-
-    @Override
-    public int getPermissionsRequestCode() {
-        //设置权限请求requestCode，只有不跟onRequestPermissionsResult方法中的其他请求码冲突即可。
-        return 10000;
-    }
-
-    @Override
-    public String[] getPermissions() {
-        return new String[]{
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        };
-    }
-
-    @Override
-    public void requestPermissionsSuccess() {
-        //所有权限都已满足，可以进行界面操作
-        initView(); //初始化界面引用
-        initLocationService(); //初始化定位服务
-    }
-
-    @Override
-    public void requestPermissionsFail() {
-        //权限未满足
-        Toast.makeText(this, "未满足所有权限，无法使用", Toast.LENGTH_SHORT).show();
-        finish();
-    }
-    // --------------------- 动态权限校验回调结束 ---------------------
 }
